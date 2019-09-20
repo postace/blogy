ddde9df97d8dd3f04834699149e0afb47411357c
hai.phamthanh <hai.phamthanh@vndirect.com.vn>
2019-09-20 22:23:36 +0700

    Add API login to allow user register via Google

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,4 +1,7 @@
 from flask import jsonify, request
+import requests
+from ..models import User
+from .. import db
 from . import api
 
 
@@ -9,13 +12,34 @@ def before_request():
     print("Incoming request path ", request.path)
 
 
+class LoginType:
+    FACEBOOK = 'FACEBOOK'
+    GOOGLE = 'GOOGLE'
+
+
+GOOGLE_OAUTH2_API_URL = 'https://oauth2.googleapis.com'
+
+
 @api.route('/auth/login', methods=['POST'])
 def login_or_register():
-    # Several job to be done here
-    # Check type is facebook/google
-    # Make an api request to two of these
-    # Received data back
-    # Check email
-    # If exists -> then create token and return along with user information
-    # Otherwise -> create user, create token and return information
+    req_body = request.json
+    login_type = req_body.get('login_type')
+    token = req_body.get('token')
+
+    if login_type.upper() == LoginType.GOOGLE:
+        google_res = requests.get(GOOGLE_OAUTH2_API_URL +
+                                  "/tokeninfo?id_token=" + token)
+        google_user = google_res.json()
+        # Check email
+        google_email = google_user.get('email', '')
+        user = User.query.filter_by(email=google_email.lower()).first()
+        if user:
+            return jsonify({'message': 'User with this email already exist'})
+        else:
+            # Save user to database
+            user = User(email=google_email)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'user': user.to_json()})
+
     return jsonify({'message': 'ok'})
--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ from . import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    # Change type from String to Unicode
     email = db.Column(db.String(256), unique=True, index=True)
     name = db.Column(db.String(64))
     phone_number = db.Column(db.String(16))
@@ -14,3 +13,13 @@ class User(db.Model):
     member_from = db.Column(db.String(16))
     member_since = db.Column(db.DateTime(), default=datetime.utcnow())
     has_enough_info = db.Column(db.Boolean, default=False)
+
+    def to_json(self):
+        json_user = {
+            'id': self.id,
+            'email': self.email,
+            'name': self.name,
+            'member_since': self.member_since,
+            'has_enough_info': self.has_enough_info
+        }
+        return json_user

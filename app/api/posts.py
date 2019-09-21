4b2b6fd7de39482a38973ea7435d71b488ef2f47
hai.phamthanh <hai.phamthanh@vndirect.com.vn>
2019-09-21 14:46:05 +0700

    Add API to get a post by id

--- app/api/posts.py
+++ app/api/posts.py
@@ -5,6 +5,12 @@ from .. import db
 from ..models import Post
 
 
+@api.route('/posts/<int:id>')
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
+
+
 @api.route('/posts', methods=['POST'])
 def new_post():
     json_post = request.json

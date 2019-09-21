a53156e70bf98feb1550d5cebbc3a67a8fde74f5
hai.phamthanh <hai.phamthanh@vndirect.com.vn>
2019-09-21 09:58:24 +0700

    Return appropriate error message with http status code when login failed

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,9 +1,10 @@
-from flask import jsonify, request
 import facebook
 import requests
-from ..models import User
-from .. import db
+from flask import jsonify, request
+
 from . import api
+from .. import db
+from ..models import User
 
 
 @api.before_request
@@ -26,40 +27,55 @@ def login_or_register():
     req_body = request.json
     login_type = req_body.get('login_type')
     token = req_body.get('token')
-    # Check login_type
-    # Check token
+    if login_type is None or token is None:
+        return bad_request("login_type and token is required")
 
     if login_type.upper() == LoginType.GOOGLE:
         google_res = requests.get(GOOGLE_OAUTH2_API_URL +
                                   "/tokeninfo?id_token=" + token)
         google_user = google_res.json()
         # Check email
-        google_email = google_user.get('email', '')
+        google_email = google_user.get('email', None)
         user = User.query.filter_by(email=google_email.lower()).first()
         if user:
-            return jsonify({'message': 'User with this email already exist'})
+            return conflict('An account with this email already exist')
         else:
-            # Save user to database
-            user = User(email=google_email)
+            user = User(email=google_email, member_from=LoginType.GOOGLE)
+            user.gg_id = google_user.get('sub', '')
+
             db.session.add(user)
             db.session.commit()
-            return jsonify({'user': user.to_json()})
+            return jsonify({
+                'user': user.to_json()
+            })
     elif login_type.upper() == LoginType.FACEBOOK:
-
         graph = facebook.GraphAPI(access_token=token, version='3.1')
         facebook_user = graph.get_object('me?fields=id,name,email')
         # Check email
-        facebook_email = facebook_user.get('email', '')
+        facebook_email = facebook_user.get('email', None)
         user = User.query.filter_by(email=facebook_email.lower()).first()
         if user:
-            return jsonify({'message': 'User with this email already exist'})
+            return conflict('An account with this email already exist')
         else:
-            # Save user to database
-            user = User(email=facebook_email)
+            user = User(email=facebook_email, member_from=LoginType.FACEBOOK)
+            user.fb_id = facebook_user.get('id', '')
+
             db.session.add(user)
             db.session.commit()
-            return jsonify({'user': user.to_json()})
+            return jsonify({
+                'user': user.to_json()
+            })
+    else:
+        return bad_request('Unknown login type. Must be facebook or google')
 
 
-    # Throw error here because login-type is not defined
-    return jsonify({'message': 'ok'})
+def bad_request(message):
+    response = jsonify({'error': 'bad request', 'message': message})
+    response.status_code = 400
+    return response
+
+
+def conflict(message):
+    response = jsonify({'error': 'conflict', 'message': message})
+    response.status_code = 409
+    return response

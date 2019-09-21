a8d18d917323b4f0831edb39e7f2e434d02befd4
hai.phamthanh <hai.phamthanh@vndirect.com.vn>
2019-09-21 22:10:52 +0700

    Refactor extract method in login_or_register()

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import (create_access_token,
                                 get_jwt_identity)
 
 from . import api
-from .errors import not_found, bad_request, conflict
+from .errors import not_found, bad_request, conflict, unauthorized
 from .. import db
 from ..models import User
 
@@ -26,7 +26,7 @@ def before_request():
         user_id = get_user_id()
         user = User.query.get(user_id)
         if not user:
-            return not_found("User not found with id {}" % user_id)
+            return not_found("User not found with id {}".format(user_id))
 
         if user_not_going_to_provide_required_info(request, user):
             return bad_request("You have to provide the required info to using this feature")
@@ -42,7 +42,7 @@ def get_user_id():
 
 def user_not_going_to_provide_required_info(req, user):
     return 'me/supply-info' not in req.path \
-            and not user.has_required_info
+           and not user.has_required_info
 
 
 @api.route('/auth/login', methods=['POST'])
@@ -56,40 +56,37 @@ def login_or_register():
     if login_type.upper() == LoginType.GOOGLE:
         google_res = requests.get(GOOGLE_OAUTH2_API_URL +
                                   "/tokeninfo?id_token=" + token)
-        google_user = google_res.json()
-        # Check email
-        google_email = google_user.get('email')
-        user = User.query.filter_by(email=google_email.lower()).first()
-        if user:
-            return conflict('An account with this email already exist')
-        else:
-            user = User(email=google_email.lower(), member_from=LoginType.GOOGLE)
-            user.gg_id = google_user.get('sub')
-
-            db.session.add(user)
-            db.session.commit()
-
-            return jsonify({
-                'user': user.to_json(),
-                'access_token': create_access_token(identity=user.id)
-            })
+        gg_user = google_res.json()
+        return new_user_with_type(gg_user.get('email'),
+                                  LoginType.GOOGLE,
+                                  gg_user.get('sub'))
     elif login_type.upper() == LoginType.FACEBOOK:
         graph = facebook.GraphAPI(access_token=token, version='3.1')
-        facebook_user = graph.get_object('me?fields=id,name,email')
-        # Check email
-        facebook_email = facebook_user.get('email', None)
-        user = User.query.filter_by(email=facebook_email.lower()).first()
-        if user:
-            return conflict('An account with this email already exist')
-        else:
-            user = User(email=facebook_email.lower(), member_from=LoginType.FACEBOOK)
-            user.fb_id = facebook_user.get('id')
+        fb_user = graph.get_object('me?fields=id,name,email')
 
-            db.session.add(user)
-            db.session.commit()
-            return jsonify({
-                'user': user.to_json(),
-                'access_token': create_access_token(identity=user.id)
-            })
+        return new_user_with_type(fb_user.get('email'),
+                                  LoginType.FACEBOOK,
+                                  fb_user.get('id'))
     else:
         return bad_request('Unknown login type. Must be facebook or google')
+
+
+def new_user_with_type(email, login_from, third_party_id):
+    if email is None or email == '':
+        return unauthorized('Error when login {}'.format(login_from))
+    user = User.query.filter_by(email=email.lower()).first()
+    if user:
+        return conflict('An account with email {} already exist'.format(email))
+
+    user = User(email=email.lower(), member_from=login_from)
+    if login_from == LoginType.FACEBOOK:
+        user.fb_id = third_party_id
+    elif login_from == LoginType.GOOGLE:
+        user.gg_id = third_party_id
+
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({
+        'user': user.to_json(),
+        'access_token': create_access_token(identity=user.id)
+    })

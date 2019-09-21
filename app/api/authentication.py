8d208199e0dd008bb7f309210d8560e3e95c719d
hai.phamthanh <hai.phamthanh@vndirect.com.vn>
2019-09-21 07:55:04 +0700

    Allow user register/login via Facebook

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request
+import facebook
 import requests
 from ..models import User
 from .. import db
@@ -25,6 +26,8 @@ def login_or_register():
     req_body = request.json
     login_type = req_body.get('login_type')
     token = req_body.get('token')
+    # Check login_type
+    # Check token
 
     if login_type.upper() == LoginType.GOOGLE:
         google_res = requests.get(GOOGLE_OAUTH2_API_URL +
@@ -41,5 +44,22 @@ def login_or_register():
             db.session.add(user)
             db.session.commit()
             return jsonify({'user': user.to_json()})
+    elif login_type.upper() == LoginType.FACEBOOK:
 
+        graph = facebook.GraphAPI(access_token=token, version='3.1')
+        facebook_user = graph.get_object('me?fields=id,name,email')
+        # Check email
+        facebook_email = facebook_user.get('email', '')
+        user = User.query.filter_by(email=facebook_email.lower()).first()
+        if user:
+            return jsonify({'message': 'User with this email already exist'})
+        else:
+            # Save user to database
+            user = User(email=facebook_email)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({'user': user.to_json()})
+
+
+    # Throw error here because login-type is not defined
     return jsonify({'message': 'ok'})
